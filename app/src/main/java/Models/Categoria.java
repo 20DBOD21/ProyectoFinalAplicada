10590aad561a77db39176f7a2e09613f27dd5efd
20DBOD21 <doliverad21_1@unc.edu.pe>
2025-04-09 22:53:33 -0500

    Añadido:
    - Modificación:
    * Clase Categoria
    * Clase Usuario
    
    - Añadido:
    * Clase CategoriaDAO
    * Clase MovimientoDAO
    * Clase UsuarioDAO

--- app/src/main/java/Models/Categoria.java
+++ app/src/main/java/Models/Categoria.java
@@ -1,4 +1,46 @@
 package Models;
 
 public class Categoria {
+    protected String nombre;
+    protected String descripcion;
+    protected String icono;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+        descripcion = "";
+        icono = "";
+    }
+
+    public Categoria(String nombre, String descripcion, String icono) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.icono = icono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
 }
--- app/src/main/java/Models/Usuario.java
+++ app/src/main/java/Models/Usuario.java
@@ -1,4 +1,89 @@
 package Models;
 
 public class Usuario {
+    protected String nombre;
+    protected String apellido;
+    protected String nickname;
+    protected String password;
+    protected String email;
+    protected String rol;
+    private boolean sincNube;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String nickname, String password, String email, String rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    public Usuario(String nombre, String apellido, String nickname, String password, String email, String rol, boolean sincNube) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.rol = rol;
+        this.sincNube = sincNube;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean isSincNube() {
+        return sincNube;
+    }
+
+    public void setSincNube(boolean sincNube) {
+        this.sincNube = sincNube;
+    }
 }
